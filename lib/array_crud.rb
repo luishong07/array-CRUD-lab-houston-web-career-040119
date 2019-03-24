54dd9ccefe0f5160e6c0d7cc878735671aeb1f8e
Luis Hong <luishong50@gmail.com>
2019-03-24 19:09:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,6 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
+  stuff = ["Chicken","Garlic","Potatoes","Greens"]
   stuff.unshift("front element")
 end
 
