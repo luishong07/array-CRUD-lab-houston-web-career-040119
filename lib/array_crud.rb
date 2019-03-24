1c2117ad5645afe9b8b4471094c6e4a22d8d7362
Luis Hong <luishong50@gmail.com>
2019-03-24 19:06:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   stuff = ["Chicken","Garlic","Potatoes","Greens"]
-  stuff << "Chocolate"
+  stuff << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
