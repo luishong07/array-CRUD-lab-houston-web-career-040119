ad5ddc0bfedea9b185d4c3a948d4e7f3037cfacd
Luis Hong <luishong50@gmail.com>
2019-03-24 19:28:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,8 +29,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  stuff=["am" , "ij","rh"]
-  stuff[0]
+  stuff = ["am" , "ij","rh"]
+  stuff[1]
 end
 
 def retrieve_first_element_from_array(array)
