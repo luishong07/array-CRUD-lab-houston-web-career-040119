b5a4e402d691e615b7be9886d55514d60deaed22
Luis Hong <luishong50@gmail.com>
2019-03-24 19:01:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+ stuff = []
 end
 
 def create_an_array
-  
+  stuff = ["Chicken","Garlic","Potatoes","Greens"]
 end
 
 def add_element_to_end_of_array(array, element)
