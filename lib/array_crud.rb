700b68ebaf8de014eea4ce60ca170e2c81f04a41
Luis Hong <luishong50@gmail.com>
2019-03-24 19:34:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  ftuff=["wow","uh-h","nope"]
+  stuff=["wow","uh-h","nope"]
+  stuff[0]
 end
 
 def retrieve_last_element_from_array(array)
